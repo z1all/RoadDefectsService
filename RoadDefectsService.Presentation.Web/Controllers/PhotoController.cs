9008ce5a6adbf3dc3abc44693369474ef81ff40c
vitaliy ivanof <ivitalij741@gmail.com>
2024-06-08 16:39:47 +0700

    Add new contollers

---
+++ RoadDefectsService.Presentation.Web/Controllers/PhotoController.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using RoadDefectsService.Presentation.Web.Attributes;
+
+namespace RoadDefectsService.Presentation.Web.Controllers
+{
+    [Route("api/photo")]
+    [ApiController]
+    public class PhotoController : ControllerBase
+    {
+        /// <summary>
+        /// Скачать фото (Не реализовано) (Не все модели указаны)
+        /// </summary>
+        /// <remarks> 
+        /// Доступ: Все
+        /// </remarks>
+        [HttpGet("{photoId}")]
+        [CustomeAuthorize]
+        public async Task<ActionResult> DownloadPhoto([FromRoute] Guid photoId)
+        {
+            return Ok();
+        }
+    }
+}
