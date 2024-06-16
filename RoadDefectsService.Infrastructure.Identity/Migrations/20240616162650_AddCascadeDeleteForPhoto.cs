d7f737bb523c9796fdda8b5d836708dff1adc082
vitaliy ivanof <ivitalij741@gmail.com>
2024-06-16 23:30:08 +0700

    Add cascade delete to photo

---
+++ RoadDefectsService.Infrastructure.Identity/Migrations/20240616162650_AddCascadeDeleteForPhoto.cs
@@ -0,0 +1,64 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace RoadDefectsService.Infrastructure.Identity.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCascadeDeleteForPhoto : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_FixationDefects_FixationDefectId",
+                table: "Photos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_FixationWorks_FixationWorkId",
+                table: "Photos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_FixationDefects_FixationDefectId",
+                table: "Photos",
+                column: "FixationDefectId",
+                principalTable: "FixationDefects",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_FixationWorks_FixationWorkId",
+                table: "Photos",
+                column: "FixationWorkId",
+                principalTable: "FixationWorks",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_FixationDefects_FixationDefectId",
+                table: "Photos");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Photos_FixationWorks_FixationWorkId",
+                table: "Photos");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_FixationDefects_FixationDefectId",
+                table: "Photos",
+                column: "FixationDefectId",
+                principalTable: "FixationDefects",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Photos_FixationWorks_FixationWorkId",
+                table: "Photos",
+                column: "FixationWorkId",
+                principalTable: "FixationWorks",
+                principalColumn: "Id");
+        }
+    }
+}
